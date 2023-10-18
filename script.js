b97ef28e5c286a4f4707c9dabb50e54388488dc5
ljenchik <pelagenkolena@gmail.com>
2023-10-18 14:27:41 +0100

    Added querySelectorAll and time/times

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-let lightbulb1 = document.querySelector("#lightbulb1");
-let lightbulb2 = document.querySelector("#lightbulb2");
-let lightbulb3 = document.querySelector("#lightbulb3");
+// let lightbulb1 = document.querySelector("#lightbulb1");
+// let lightbulb2 = document.querySelector("#lightbulb2");
+// let lightbulb3 = document.querySelector("#lightbulb3");
 
 let subtitle = document.querySelector(".subtitle");
 let count = 0;
 
-lightbulb1.addEventListener("click", function () {
-    count++;
-    subtitle.innerHTML = "You've clicked the lights " + count + " times";
-    lightbulb1.classList.toggle("active");
-});
+const bulbs = document.querySelectorAll(".item");
+for (const bulb of bulbs) {
+    bulb.addEventListener("click", function () {
+        count++;
+        if (count === 1) {
+            subtitle.innerHTML = "You've clicked the lights " + count + " time";
+        } else {
+            subtitle.innerHTML =
+                "You've clicked the lights " + count + " times";
+        }
+        bulb.classList.toggle("active");
+    });
+}
 
-lightbulb2.addEventListener("click", function () {
-    count++;
-    subtitle.innerHTML = "You've clicked the lights " + count + " times";
-    lightbulb2.classList.toggle("active");
-});
+// lightbulb1.addEventListener("click", function () {
+//     count++;
+//     subtitle.innerHTML = "You've clicked the lights " + count + " times";
+//     lightbulb1.classList.toggle("active");
+// });
 
-lightbulb3.addEventListener("click", function () {
-    count++;
-    subtitle.innerHTML = "You've clicked the lights " + count + " times";
-    lightbulb3.classList.toggle("active");
-});
+// lightbulb2.addEventListener("click", function () {
+//     count++;
+//     subtitle.innerHTML = "You've clicked the lights " + count + " times";
+//     lightbulb2.classList.toggle("active");
+// });
+
+// lightbulb3.addEventListener("click", function () {
+//     count++;
+//     subtitle.innerHTML = "You've clicked the lights " + count + " times";
+//     lightbulb3.classList.toggle("active");
+// });
